fd5ea450b66dbc6ab951e4c9631de0c219989234
Rodrigo <rodrigogaitan28@gmail.com>
2022-02-23 19:46:52 -0300

    Desafio Clase 3

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+const Nav = () => {
+    return(
+    <header>
+        <h1>caelum</h1>
+            <nav>
+                <a href="#" >Home</a>
+                <a href="#" >Products</a>
+                <a href="#" >Contact</a>
+            </nav>
+    </header>
+    )
+}
+
+export default Nav
